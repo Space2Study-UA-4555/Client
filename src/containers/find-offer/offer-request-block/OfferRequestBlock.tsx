e5a2878f51445a7e1624a284e59f1417f20959e8
andrii-hn <andrii.hnylytskyi04@gmail.com>
2026-06-29 18:55:59 +0300

    feat: implement 'create request/offer' block along with drawer

--- src/containers/find-offer/offer-request-block/OfferRequestBlock.tsx
+++ src/containers/find-offer/offer-request-block/OfferRequestBlock.tsx
@@ -1,31 +1,54 @@
 import { useTranslation } from 'react-i18next'
 
-import TitleBlock from '~/components/title-block/TitleBlock'
+import Box from '@mui/material/Box'
 import icon from '~/assets/img/find-offer/subject_icon.png'
+import TitleBlock from '~/components/title-block/TitleBlock'
 import AppButton from '~/components/app-button/AppButton'
+import AppDrawer from '~/components/app-drawer/AppDrawer'
 import useBreakpoints from '~/hooks/use-breakpoints'
 import { useDrawer } from '~/hooks/use-drawer'
+import { useAppSelector } from '~/hooks/use-redux'
 import { translationKey } from '~/containers/find-offer/constants'
+import { styles } from '~/containers/find-offer/offer-request-block/OfferRequestBlock.styles'
 
 const OfferRequestBlock = () => {
   const { t } = useTranslation()
   const { isMobile } = useBreakpoints()
-  const { openDrawer } = useDrawer()
+  const { openDrawer, closeDrawer, isOpen } = useDrawer()
+  const { userRole } = useAppSelector((state) => state.appMain)
 
   const handleOpenDrawer = () => {
     openDrawer()
   }
 
   return (
-    <TitleBlock img={icon} translationKey={translationKey}>
-      <AppButton
-        fullWidth={isMobile}
-        onClick={handleOpenDrawer}
-        sx={{ py: '14px' }}
+    <>
+      <TitleBlock img={icon} translationKey={translationKey}>
+        <AppButton
+          fullWidth={isMobile}
+          onClick={handleOpenDrawer}
+          sx={styles.button}
+        >
+          {t(`${translationKey}.button.${userRole}`)}
+        </AppButton>
+      </TitleBlock>
+
+      <AppDrawer
+        ModalProps={{
+          sx: styles.drawerModal,
+          BackdropProps: {
+            sx: styles.drawerBackdrop
+          }
+        }}
+        PaperProps={{
+          sx: styles.drawerPaper
+        }}
+        onClose={closeDrawer}
+        open={isOpen}
       >
-        {t(`${translationKey}.button`)}
-      </AppButton>
-    </TitleBlock>
+        <Box sx={styles.drawerContent}>CreateOfferDrawer</Box>
+      </AppDrawer>
+    </>
   )
 }
 
--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,7 +1,12 @@
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import OfferRequestBlock from '~/containers/find-offer/offer-request-block/OfferRequestBlock'
 
 const Categories = () => {
-  return <PageWrapper>Categories</PageWrapper>
+  return (
+    <PageWrapper>
+      <OfferRequestBlock />
+    </PageWrapper>
+  )
 }
 
 export default Categories
