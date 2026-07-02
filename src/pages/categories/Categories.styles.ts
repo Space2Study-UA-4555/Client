09c91e7ef7b3ff2dcaedd8dd1be534c24e78ac68
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2026-07-03 02:20:14 +0300

    feat(#90): update Categories page UI and fix no-results block

---
+++ src/pages/categories/Categories.styles.ts
@@ -0,0 +1,22 @@
+export const styles = {
+  navigation: {
+    display: 'flex',
+    justifyContent: 'flex-end'
+  },
+  searchToolbar: {
+    borderRadius: '70px'
+  },
+  titleWithDescription: {
+    wrapper: {
+      my: '30px',
+      textAlign: 'center'
+    },
+    title: {
+      typography: { sm: 'h4', xs: 'h5' }
+    },
+    description: {
+      typography: { sm: 'body1', xs: 'body2' },
+      color: 'primary.500'
+    }
+  }
+}
--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,7 +1,167 @@
+import { useCallback, useMemo, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import type { SxProps } from '@mui/material'
+import Box from '@mui/material/Box'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+
+import { useAppSelector } from '~/hooks/use-redux'
+import useLoadMore from '~/hooks/use-load-more'
+import useCategoriesNames from '~/hooks/use-categories-names'
+import { categoryService } from '~/services/category-service'
+import { useModalContext } from '~/context/modal-context'
+
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import SearchAutocomplete from '~/components/search-autocomplete/SearchAutocomplete'
+import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
+import NotFoundResults from '~/components/not-found-results/NotFoundResults'
+import CardsList from '~/components/cards-list/CardsList'
+import CardWithLink from '~/components/card-with-link/CardWithLink'
+import DirectionLink from '~/components/direction-link/DirectionLink'
+import CreateSubjectModal from '~/containers/find-offer/create-new-subject/CreateNewSubject'
+import AppToolbar from '~/components/app-toolbar/AppToolbar'
+import OfferRequestBlock from '~/containers/find-offer/offer-request-block/OfferRequestBlock'
+import useBreakpoints from '~/hooks/use-breakpoints'
+import serviceIcon from '~/assets/img/student-home-page/service_icon.png'
+import { getOpositeRole, getScreenBasedLimit } from '~/utils/helper-functions'
+
+import { CategoryInterface, SizeEnum } from '~/types'
+import { itemsLoadLimit } from '~/constants'
+import { authRoutes } from '~/router/constants/authRoutes'
+import { styles } from '~/pages/categories/Categories.styles'
 
 const Categories = () => {
-  return <PageWrapper>Categories</PageWrapper>
+  const [match, setMatch] = useState<string>('')
+  const [isFetched, setIsFetched] = useState<boolean>(false)
+  const params = useMemo(() => ({ name: match }), [match])
+
+  const { t } = useTranslation()
+  const { userRole } = useAppSelector((state) => state.appMain)
+  const breakpoints = useBreakpoints()
+  const { openModal } = useModalContext()
+
+  const cardsLimit = getScreenBasedLimit(breakpoints, itemsLoadLimit)
+
+  const categoriesStyles = styles as {
+    navigation: SxProps
+    titleWithDescription: {
+      wrapper?: SxProps
+      title?: SxProps
+      description?: SxProps
+    }
+    searchToolbar: SxProps
+  }
+
+  const {
+    loading: categoryNamesLoading,
+    response: categoriesNamesItems,
+    fetchData
+  } = useCategoriesNames({
+    fetchOnMount: false
+  })
+
+  const categoryNamesOptions = useMemo(
+    () =>
+      (categoriesNamesItems || []).map((item) =>
+        typeof item === 'string' ? item : item.name
+      ),
+    [categoriesNamesItems]
+  )
+
+  const getCategoryNames = () => {
+    !isFetched && void fetchData()
+    setIsFetched(true)
+  }
+
+  const getCategories = useCallback(
+    (data?: Pick<CategoryInterface, 'name'>) =>
+      categoryService.getCategories(data),
+    []
+  )
+
+  const {
+    data: categories,
+    loading: categoriesLoading,
+    resetData,
+    loadMore,
+    isExpandable
+  } = useLoadMore<CategoryInterface, Pick<CategoryInterface, 'name'>>({
+    service: getCategories,
+    limit: cardsLimit,
+    params
+  })
+
+  const oppositeRole = getOpositeRole(userRole)
+
+  const cards = useMemo(
+    () =>
+      categories.map((item: CategoryInterface) => {
+        return (
+          <CardWithLink
+            description={`${item.totalOffers[oppositeRole]} ${t(
+              'categoriesPage.offers'
+            )}`}
+            img={serviceIcon}
+            key={item._id}
+            link={`${authRoutes.subjects.path}?categoryId=${item._id}`}
+            title={item.name}
+          />
+        )
+      }),
+    [categories, oppositeRole, t]
+  )
+
+  const handleOpenModal = () => openModal({ component: <CreateSubjectModal /> })
+
+  return (
+    <PageWrapper>
+      <OfferRequestBlock />
+
+      <TitleWithDescription
+        description={t('categoriesPage.description')}
+        style={categoriesStyles.titleWithDescription}
+        title={t('categoriesPage.title')}
+      />
+
+      <Box sx={categoriesStyles.navigation}>
+        <DirectionLink
+          after={<ArrowForwardIcon fontSize={SizeEnum.Small} />}
+          linkTo={authRoutes.findOffers.path}
+          title={t('categoriesPage.showAllOffers')}
+        />
+      </Box>
+
+      <AppToolbar sx={categoriesStyles.searchToolbar}>
+        <SearchAutocomplete
+          loading={categoryNamesLoading}
+          onFocus={getCategoryNames}
+          onSearchChange={resetData}
+          options={categoryNamesOptions}
+          search={match}
+          setSearch={setMatch}
+          textFieldProps={{
+            label: t('categoriesPage.searchLabel')
+          }}
+        />
+      </AppToolbar>
+
+      {!categories.length && !categoriesLoading ? (
+        <NotFoundResults
+          buttonText={t('categoriesPage.requestNewCategory')}
+          description={t('categoriesPage.noResultsDescription')}
+          onClick={handleOpenModal}
+        />
+      ) : (
+        <CardsList
+          btnText={t('categoriesPage.viewMore')}
+          cards={cards}
+          isExpandable={isExpandable}
+          loading={categoriesLoading}
+          onClick={loadMore}
+        />
+      )}
+    </PageWrapper>
+  )
 }
 
 export default Categories
